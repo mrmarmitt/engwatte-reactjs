fbc5a78200b014ce3ca8c109eb4b21d5151f2b95
Mateus Rufino Marmitt <mrmarmitt@gmail.com>
2025-05-19 11:53:58 -0300

    adding logos on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,18 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-logo">
-          <img src="/vite.svg" alt="WATTE Engenharia Logo" />
-          <p>WATTE ENGENHARIA</p>
+          <img src="/footer-logo-eng-watte.png" alt="WATTE Engenharia Logo" />
         </div>
         <div className="footer-contact">
-          <a href="mailto:info@meusite.com"><i className="fa fa-envelope"></i></a>
-          <a href="https://wa.me/1234567890"><i className="fa fa-whatsapp"></i></a>
-          <a href="https://instagram.com"><i className="fa fa-instagram"></i></a>
+          <a href="mailto:info@meusite.com">
+            <img src="/email.svg" alt="Email" style={{ width: '80px', height: '80px', background: 'transparent' }}/>
+          </a>
+          <a href="https://wa.me/1234567890">
+            <img src="/whatsApp.svg" alt="WhatsApp" style={{ width: '50px', height: '50px', background: 'transparent' }}/>
+          </a>
+          <a href="https://instagram.com">
+            <img src="/instagram.svg" alt="Instagram" style={{ width: '80px', height: '80px', background: 'transparent' }}/>
+          </a>
         </div>
         <div className="footer-address">
           <p>Azaleia e Cia Ltda. - CPF/CNPJ: 12.345.678/0000-01</p>
