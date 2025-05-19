3c97afeeec903b031f067af09f07ca209b94fb42
Mateus Rufino Marmitt <mrmarmitt@gmail.com>
2025-05-19 15:24:14 -0300

    adding backgorund

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <main>
+      {/* <h1>Welcome to our website!</h1>
+      <p>This is the main content of our site.</p> */}
+    </main>
+  );
+}
+
+export default Main;
